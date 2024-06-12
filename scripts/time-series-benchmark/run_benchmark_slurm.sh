1c2a4127336704c761cecd69a5a883028bf8c631
hoos <hoos@tf.uni-freiburg.de>
2024-06-12 10:10:21 +0200

    Add debugging features

--- scripts/time-series-benchmark/run_benchmark_slurm.sh
+++ scripts/time-series-benchmark/run_benchmark_slurm.sh
@@ -5,14 +5,15 @@
 #SBATCH --job-name=time_series_benchmark
 #SBATCH --nodes=1                             # Number of nodes
 #SBATCH --gres=gpu:1                          # Number of GPUs
-#SBATCH --ntasks-per-node=1                   # Number of tasks per node
-#SBATCH --cpus-per-task=4                     # Number of CPU cores per task
-#SBATCH --mem=2G                              # Total memory limit
-#SBATCH --array=1-29                          # Total number of datasets in the benchmark
+#SBATCH --ntasks-per-node=1                   # Number of tasks per nosqde
+#SBATCH --cpus-per-task=16                    # Number of CPU cores per task
+#SBATCH --mem=32G                             # Total memory limit
+#SBATCH --array=0-28                          # Total number of datasets in the benchmark
 #SBATCH --output=slurm_out/job_output_%j.txt  # Standard output and error log (%j expands to jobId)
 
 # Load any modules or source your environment here if necessary
 #module load python
+source ~/.time_bashrc
 
 # Run the experiment script with the SLURM_ARRAY_TASK_ID as an argument
 AUTOMLBENCHMARK_CONFIG_PATH="$HOME/.config/automlbenchmark/benchmarks"
@@ -20,6 +21,8 @@ ID_TO_TASK_MAPPING_PATH=$AUTOMLBENCHMARK_CONFIG_PATH/"id_to_task_mapping.yaml"
 
 FRAMEWORK=$1
 BENCHMARK=$2
+CONSTRAINT="4h16c"
+
 TASK_NAME=$(python <<EOF
 import yaml
 with open('$ID_TO_TASK_MAPPING_PATH', 'r') as file:
@@ -30,4 +33,9 @@ EOF
 WANDB_PROJECT="tabpfn-time-series"
 
 echo "Running benchmark on task $TASK_NAME"
-python runbenchmark.py $FRAMEWORK $BENCHMARK -t $TASK_NAME --wandb_project $WANDB_PROJECT
+python runbenchmark.py $FRAMEWORK $BENCHMARK $CONSTRAINT \
+  -t $TASK_NAME \
+  --wandb_project $WANDB_PROJECT \
+  --wandb_tags $CONSTRAINT \
+  --wandb_group_id $SLURM_ARRAY_JOB_ID \
+  --debug_jid $SLURM_JOB_ID
